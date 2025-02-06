8ad95062a876f7f36725cac39ea5cffd77d40199
juan10-7 <juansantana1@colegioguanenta.edu.co>
2025-02-06 16:16:19 -0500

    se actualizaron archivos

---
+++ minicalculadora_guanentina.py
@@ -0,0 +1,31 @@
+x=int(input("ingrese un numero : "))
+
+y=int(input("ingrese otro numero: "))
+
+opc=int(input("ingrese una opc: "))
+
+##procesing
+
+if opc==1:
+    print(x+y) #output
+
+if opc==2:
+    print(x-y) #output
+
+if opc==3:
+    print(x*y) #output
+
+if opc==4:
+    print(x/y) #output
+
+if opc==5:
+    print(xŷ) #output
+
+
+
+
+
+
+
+
+
